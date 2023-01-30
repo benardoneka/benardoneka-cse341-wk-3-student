6b2be7bb46d2b5baf7f8043118c806dab84fe340
Benard Oneka <benardoneka@gmail.com>
2023-01-30 22:14:20 +0300

    First commit Put and Delete

--- controllers/employee.js
+++ controllers/employee.js
@@ -1,5 +1,6 @@
 const mongodb = require("../db/connect");
 const ObjectId = require("mongodb").ObjectId;
+const { check, validationResult } = require('express-validator');
 
 
 const getAllEmployee = async (req, res, next) => {
@@ -15,26 +16,114 @@ const getAllEmployee = async (req, res, next) => {
     });
   };
 
-// Using a post request
+// Function that uses post request to create an employee record
 const createEmployee = async (req, res, next) => {
-    const data = req.body;
-    const result = await mongodb
-      .getDb()
-      .db("cse341-wk3-l05")
-      .collection("employee")
-      .insertOne(data)
-      .then((result) => {
-        res.setHeader("Content-Type", "application/json");
-        res.status(200).json(result);
-      })
-      .catch((err) => {
-        res.status(500).json({ err: "Could not create contact mongodb!" });
-      });
-  };
+  const data = req.body;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    await check('firstName', 'First name is required').not().isEmpty().run(req);
+    await check('lastName', 'Last name is required').not().isEmpty().run(req);
+    await check('email', 'Email is required').not().isEmpty().run(req);
+    await check('gender', 'Gender is required').not().isEmpty().isIn(['Male', 'male', 'Female', 'female']).run(req);
+    await check('birthday', 'Birthday is required').not().isEmpty().run(req);
+    await check('age', 'Age is required').not().isEmpty().run(req);
+    await check('year', 'Year is required').not().isEmpty().run(req);
+    await check('semester', 'Semester is required').not().isEmpty().run(req);
+    await check('grade', 'Grade is required').not().isEmpty().run(req);
+  } catch (error) {
+    return res.status(400).json({ message: error.errors[0].msg});
+  }
+
+  const result = await mongodb
+    .getDb()
+    .db("cse341-wk3-l05")
+    .collection("employee")
+    .insertOne(data)
+    .then((result) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Could not create employee in MongoDB" });
+    });
+};
+
+//Updating a field using put request API
+const updateEmployee = async (req, res, next) => {
+  const data = req.body;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  
+  try {
+    
+    const userId = new ObjectId(req.params.id);
+    const data = req.body; 
+
+    await check('firstName', 'First name is required').not().isEmpty().run(req);
+    await check('lastName', 'Last name is required').not().isEmpty().run(req);
+    await check('email', 'Email is required').not().isEmpty().run(req);
+    await check('gender', 'Gender is required').not().isEmpty().isIn(['Male', 'male', 'Female', 'female']).run(req);
+    await check('birthday', 'Birthday is required').not().isEmpty().run(req);
+    await check('age', 'Age is required').not().isEmpty().run(req);
+    await check('year', 'Year is required').not().isEmpty().run(req);
+    await check('semester', 'Semester is required').not().isEmpty().run(req);
+    await check('grade', 'Grade is required').not().isEmpty().run(req);
+
+    const db = await mongodb.getDb();
+    const collection = db.db("cse341-wk3-l05").collection("employee");
+    const filter = { _id: userId };
+    const update = { $set: data };
+    const options = { upsert: false };
+    const result = await collection.updateOne(filter, update, options);
+
+    if (result.modifiedCount === 0) {
+      return res.status(404).json({ message: `Employee not found by this Id No: ${userId }`});
+    }
+
+    res.setHeader("Content-Type", "application/json");
+    res
+      .status(200)
+      .json({ message: `${result.modifiedCount} Employee records updated` }); 
+  } catch (err) {
+    res.status(500).json({ message: "Could not update Employee in MongoDB" });
+    return res.status(400).json({ message: err.errors[0].msg});
+  }
+};
+
+// Deleting a record using rest API
+const deleteEmployee = async (req, res, next) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+
+    const db = await mongodb.getDb();
+    const collection = db.db("cse341-wk3-l05").collection("employee");
+    const filter = { _id: userId };
+    const result = await collection.deleteOne(filter);
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: `Employee record by this ID No: ${userId} not found` });
+    }
+
+    res.status(200).json({ message: `${result.deletedCount} Employee record by this ID No: ${userId} deleted` });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Could not delete contact in MongoDB" });
+  }
+};
 
 
   module.exports = {
     getAllEmployee,
-    createEmployee
+    createEmployee,
+    updateEmployee,
+    deleteEmployee
 
   };
--- routes/employee.js
+++ routes/employee.js
@@ -2,11 +2,37 @@ const express = require("express");
 const router = express.Router();
 const app = express();
 app.use(express.json());
-
 const employeeController = require("../controllers/employee");
 
+const { check, validationResult } = require('express-validator');
+
 router.get("/", employeeController.getAllEmployee);
-router.post("/", employeeController.createEmployee);
+
+router.post("/", [
+    check('firstName', 'First name is required').not().isEmpty(),
+    check('lastName', 'Last name is required').not().isEmpty(),
+    check('email', 'Email is required').not().isEmpty(),
+    check('gender', 'Gender is required').not().isEmpty().isIn(['Male', 'male', 'Female', 'female']),
+    check('birthday', 'Birthday is required').not().isEmpty(),
+    check('age', 'Age is required').not().isEmpty(),
+    check('year', 'Year is required').not().isEmpty(),
+    check('semester', 'Semester is required').not().isEmpty(),
+    check('grade', 'Grade is required').not().isEmpty(),
+  ], employeeController.createEmployee);
+
+router.put("/:id", [
+    check('firstName', 'First name is required').not().isEmpty(),
+    check('lastName', 'Last name is required').not().isEmpty(),
+    check('email', 'Email is required').not().isEmpty(),
+    check('gender', 'Gender is required').not().isEmpty().isIn(['Male', 'male', 'Female', 'female']),
+    check('birthday', 'Birthday is required').not().isEmpty(),
+    check('age', 'Age is required').not().isEmpty(),
+    check('year', 'Year is required').not().isEmpty(),
+    check('semester', 'Semester is required').not().isEmpty(),
+    check('grade', 'Grade is required').not().isEmpty(),
+], employeeController.updateEmployee);
+
+router.delete("/:id", employeeController.deleteEmployee);
 
 
 module.exports = router;
